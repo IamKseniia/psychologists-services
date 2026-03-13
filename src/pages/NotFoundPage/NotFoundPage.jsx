187bdf89ed8ab74e5ec431e2c1ec691d89d38e07
Kseniia Safronova <yakseniya1994@gmail.com>
2026-03-13 18:05:27 +0100

    add changes in NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom';
-import s from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
   return (
-    <div className={s.wrapper}>
-      <p className={s.text}>
+    <div>
+      <p>
         Sorry, page not found! Please go to{' '}
-        <span className={s.home}>
+        <span>
           <Link to="/">Home page</Link>
         </span>
       </p>
